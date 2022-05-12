3574e85152efa1492b121b61a68b42b2ca16629f
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-05-12 17:20:49 -0300

    .

--- src/modules/accounts/repositories/infra/UsersRepository.ts
+++ src/modules/accounts/repositories/infra/UsersRepository.ts
@@ -1,29 +1,35 @@
-import { prisma } from "@shared/database/prismaClient";
-
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
 
 import { User } from "@prisma/client";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
+import { prisma } from "../../../../shared/database/prismaClient";
+
 export class UsersRepository implements IUsersRepository {
-    
+    findByEmail(email: string): Promise<User> {
+        throw new Error("Method not implemented.");
+    }
+
+    findById(id: string): Promise<User> {
+        throw new Error("Method not implemented.");
+    }
+
     async create({
         name,
         password,
         email,
-        avatar_url?,
-        role?,
+        avatar_url,
+        role,
     }: ICreateUserDTO): Promise<void> {
-        const user = await prisma.user.create({
-            name,
-            password,
-            email,
-            avatar_url,
-            role
+        await prisma.user.create({
+            data: {
+                name,
+                password,
+                email,
+                avatar_url,
+                role,
+            },
         });
     }
-
-
 }
-

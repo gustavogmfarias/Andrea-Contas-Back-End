870161ae0f3c8fd1aa1366f51f411eb19796cb44
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-05-13 16:38:34 -0300

    .

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -19,13 +19,13 @@ class UpdateUserAvatarUseCase {
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
 
-        if (user.avatar) {
-            await this.storageProvider.delete(user.avatar, "avatar");
+        if (user?.avatar_url) {
+            await this.storageProvider.delete(user.avatar_url, "avatar");
         }
 
         await this.storageProvider.save(avatar_file, "avatar");
 
-        user.avatar = avatar_file;
+        user?.avatar_url = avatar_file;
 
         await this.usersRepository.create(user);
     }

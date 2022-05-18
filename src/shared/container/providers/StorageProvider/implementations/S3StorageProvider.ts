4292dd8ca7898b6228864aecd924dfcea25b117b
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-05-18 15:01:50 -0300

    .

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -17,7 +17,7 @@ class S3StorageProvider implements IStorageProvider {
 
         const fileContent = await fs.promises.readFile(originalName);
 
-        const ContentType = mime.lookup(originalName);
+        const ContentType = mime.getType(originalName);
 
         await this.client
             .putObject({

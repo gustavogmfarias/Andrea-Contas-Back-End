0f02bd29310ba001d183e5b5f1b0ca6a7bebaca1
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-05-25 18:49:30 -0300

    .

--- src/modules/accounts/mapper/UserMap.ts
+++ src/modules/accounts/mapper/UserMap.ts
@@ -3,12 +3,19 @@ import { instanceToInstance } from "class-transformer";
 import { IUserResponseDTO } from "../dtos/IUserResponseDTO";
 
 class UserMap {
-    static toDTO({ email, name, id, avatar_url }: User): IUserResponseDTO {
+    static toDTO({
+        email,
+        name,
+        id,
+        avatar_url,
+        role,
+    }: User): IUserResponseDTO {
         const user = instanceToInstance({
             email,
             name,
             id,
             avatar_url,
+            role,
         });
         return user;
     }

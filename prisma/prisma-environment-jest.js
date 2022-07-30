38597c4a4fdb7aa9c95916c1613303ac15a5f2a4
Gustavo <gustavogmfarias@gmail.com>
2022-07-30 12:15:47 -0300

    Corrigido bug test

--- prisma/prisma-environment-jest.js
+++ prisma/prisma-environment-jest.js
@@ -4,6 +4,7 @@ const { v4: uuid } = require("uuid");
 const { execSync } = require("child_process");
 const { resolve } = require("path");
 const { Client } = require("pg");
+const { setTimeout } = require("timers/promises");
 
 const prismaCli = "./node_modules/.bin/prisma";
 
@@ -14,8 +15,12 @@ require("dotenv").config({
 class CustomEnvironment extends NodeEnvironment {
     constructor(config) {
         super(config);
+        process.env.DATABASE_URL =
+            "postgresql://docker:ignite@localhost:8083/andrea_contas_test?schema=";
         this.schema = `code_schema_${uuid().slice(0, 5)}`;
         console.log("schemas", this.schema);
+        this.connectionString = `${process.env.DATABASE_URL}`;
+
         this.connectionString = `${process.env.DATABASE_URL}${this.schema}`;
     }
 

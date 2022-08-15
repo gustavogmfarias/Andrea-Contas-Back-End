cd48a28a5d4c283e53e40e0043390685e8c4cd8e
Gustavo <gustavogmfarias@gmail.com>
2022-08-15 14:59:16 -0300

    listar clientes

--- src/modules/contas/useCases/ListContasUseCase/tests/ListContasController.spec.ts
+++ src/modules/contas/useCases/ListContasUseCase/tests/ListContasController.spec.ts
@@ -6,6 +6,8 @@ import { DayjsDateProvider } from "@shared/container/providers/DateProvider/impl
 import { app } from "@shared/infra/http/app";
 import request from "supertest";
 
+/* eslint-disable prefer-destructuring */
+
 describe("CLIENTE - List Clientes Controller", () => {
     const dateProvider = new DayjsDateProvider();
     const dataAtual = dateProvider.dateNow();
@@ -16,15 +18,19 @@ describe("CLIENTE - List Clientes Controller", () => {
     let clienteBodyA;
     let clienteBodyB;
     let clienteBodyC;
-    let clienteBBody;
 
     let contaA;
+    let contaBodyA;
     let contaB;
+    let contaBodyB;
     let contaC;
+    let contaBodyC;
     let contaD;
+    let contaBodyD;
     let contaE;
+    let contaBodyE;
     let contaF;
-    let lojistaId;
+    let contaBodyF;
 
     beforeAll(async () => {
         const lojista = await request(app)
@@ -51,7 +57,6 @@ describe("CLIENTE - List Clientes Controller", () => {
             })
             .set({ Authorization: `Bearer ${lojistaToken}` });
 
-        // eslint-disable-next-line prefer-destructuring
         clienteBodyA = clienteA.body[0];
 
         clienteB = await request(app)
@@ -72,7 +77,6 @@ describe("CLIENTE - List Clientes Controller", () => {
             })
             .set({ Authorization: `Bearer ${lojistaToken}` });
 
-        // eslint-disable-next-line prefer-destructuring
         clienteBodyB = clienteB.body[0];
 
         clienteC = await request(app)
@@ -93,7 +97,6 @@ describe("CLIENTE - List Clientes Controller", () => {
             })
             .set({ Authorization: `Bearer ${lojistaToken}` });
 
-        // eslint-disable-next-line prefer-destructuring
         clienteBodyC = clienteC.body[0];
 
         contaA = await request(app)
@@ -102,36 +105,114 @@ describe("CLIENTE - List Clientes Controller", () => {
                 observacoes: "ContaA",
                 numeroParcelas: 5,
                 valorInicial: 100,
-                dataVencimentoInicial: dateProvider.dateNow(),
+                dataVencimentoInicial: dateProvider.addDays(3),
                 fkIdCliente: clienteBodyA.id,
             })
             .set({ Authorization: `Bearer ${lojistaToken}` });
 
-        const contaBodyA = contaA.body[0];
+        contaBodyA = contaA.body[0];
 
-        const lojistaBuscadoPorUsername = await request(app)
-            .get(`/lojistas/findbyusername/`)
+        contaB = await request(app)
+            .post("/contas")
             .send({
-                username: "admin",
+                observacoes: "ContaB",
+                numeroParcelas: 10,
+                valorInicial: 100,
+                dataVencimentoInicial: dateProvider.addDays(2),
+                fkIdCliente: clienteBodyB.id,
             })
             .set({ Authorization: `Bearer ${lojistaToken}` });
 
-        lojistaId = lojistaBuscadoPorUsername.body.id;
+        contaBodyB = contaB.body[0];
+
+        contaC = await request(app)
+            .post("/contas")
+            .send({
+                observacoes: "ContaC",
+                numeroParcelas: 10,
+                valorInicial: 100,
+                dataVencimentoInicial: dateProvider.addDays(1),
+                fkIdCliente: clienteBodyC.id,
+            })
+            .set({ Authorization: `Bearer ${lojistaToken}` });
+
+        contaBodyC = contaC.body[0];
+
+        contaD = await request(app)
+            .post("/contas")
+            .send({
+                observacoes: "ContaD",
+                numeroParcelas: 10,
+                valorInicial: 80,
+                dataVencimentoInicial: dateProvider.addDays(4),
+                fkIdCliente: clienteBodyA.id,
+            })
+            .set({ Authorization: `Bearer ${lojistaToken}` });
+
+        contaBodyD = contaD.body[0];
+
+        contaE = await request(app)
+            .post("/contas")
+            .send({
+                observacoes: "ContaE",
+                numeroParcelas: 10,
+                valorInicial: 80,
+                dataVencimentoInicial: dateProvider.addDays(5),
+                fkIdCliente: clienteBodyB.id,
+            })
+            .set({ Authorization: `Bearer ${lojistaToken}` });
+
+        contaBodyE = contaE.body[0];
+
+        contaF = await request(app)
+            .post("/contas")
+            .send({
+                observacoes: "ContaF",
+                numeroParcelas: 10,
+                valorInicial: 80,
+                dataVencimentoInicial: dateProvider.addDays(-1),
+                fkIdCliente: clienteBodyA.id,
+            })
+            .set({ Authorization: `Bearer ${lojistaToken}` });
+
+        contaBodyF = contaF.body[0];
+        // Cliente A: 2 conta
+        // Cliente B: 2 conta
+        // Cliente C: 1 conta
+        // ContaF: erro da ta anterior
     });
 
-    it("Deve ser capaz de listar todos as clientes", async () => {
-        const responseToken = await request(app)
-            .post("/sessions")
-            .send({ username: "admin", senha: "admin" });
-
-        const { token } = responseToken.body;
-
+    it("Deve ser capaz de listar todas as contas", async () => {
         const contas = await request(app)
             .get("/contas")
-            .set({ Authorization: `Bearer ${token}` });
+            .set({ Authorization: `Bearer ${lojistaToken}` });
 
         expect(contas.status).toBe(200);
-        expect(contas.body).toHaveLength(2);
+        expect(contas.body).toHaveLength(5);
+    });
+
+    it("Deve ser capaz de listar as contas por cliente", async () => {
+        const contasA = await request(app)
+            .get("/contas")
+            .send({ cliente: clienteBodyA.id })
+            .set({ Authorization: `Bearer ${lojistaToken}` });
+
+        const contasB = await request(app)
+            .get("/contas")
+            .send({ cliente: clienteBodyB.id })
+            .set({ Authorization: `Bearer ${lojistaToken}` });
+
+        const contasC = await request(app)
+            .get("/contas")
+            .send({ cliente: clienteBodyC.id })
+            .set({ Authorization: `Bearer ${lojistaToken}` });
+
+        expect(contasA.status).toBe(200);
+        expect(contasA.body).toHaveLength(2);
+        expect(contasB.status).toBe(200);
+        expect(contasB.body).toHaveLength(2);
+        expect(contasC.status).toBe(200);
+        expect(contasC.body).toHaveLength(1);
     });
 
     // it("Não deve ser capaz de listar os clientes se não estiver logado", async () => {

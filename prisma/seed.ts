58248d5b37eb621de3c6d7488c095ed68d040dc5
Gustavo <gustavogmfarias@gmail.com>
2022-08-15 15:43:42 -0300

    Atualizado seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,36 +1,20 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import { hash } from "bcryptjs";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 
 const prisma = new PrismaClient();
+const dateProvider = new DayjsDateProvider();
 
 async function main() {
     const passwordAdmin = await hash("admin", 12);
     const passwordLojista = await hash("gustavo", 12);
 
-    const lojistaData: Prisma.LojistaCreateManyInput[] = [
-        {
-            username: "admin",
-            nome: "admin",
-            senha: passwordAdmin,
-        },
-        {
-            username: "gustavo",
-            nome: "gustavo",
-            senha: passwordLojista,
-        },
-    ];
-
-    console.log(`Start seeding ...`);
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const u of lojistaData) {
-        // eslint-disable-next-line no-await-in-loop
-        const lojista = await prisma.lojista.create({
-            data: u,
-        });
-
-        console.log(`Created lojista with id: ${lojista.id}`);
-    }
+    const lojista1 = await prisma.lojista.create({
+        data: { username: "admin", nome: "admin", senha: passwordAdmin },
+    });
+    const lojista2 = await prisma.lojista.create({
+        data: { username: "gustavo", nome: "gustavo", senha: passwordLojista },
+    });
 
     const cliente1 = await prisma.cliente.create({
         data: {
@@ -71,6 +55,48 @@ async function main() {
                     numero: "333",
                 },
             },
+        },
+    });
+
+    const conta1 = await prisma.conta.create({
+        data: {
+            criadoEm: this.dateProvider.convertToUtc3Hours(new Date()),
+            editadoEm: this.dateProvider.convertToUtc3Hours(new Date()),
+            observacoes: "Conta 1",
+            numeroParcelas: 12,
+            numeroParcelasAtual: 12,
+            valorInicial: 120,
+            valorParcela: 10,
+            valorAtual: 120,
+            dataVencimentoInicial: this.dateProvider.addDays(30),
+            dataVencimentoAtual: this.dateProvider.addDays(30),
+            dataVencimentoFinal: this.dateProvider.addMonths(
+                this.dateProvider.addDays(30),
+                12
+            ),
+            fkIdLojista: lojista1.id,
+            fkIdCliente: cliente1.id,
+        },
+    });
+
+    const conta2 = await prisma.conta.create({
+        data: {
+            criadoEm: this.dateProvider.convertToUtc3Hours(new Date()),
+            editadoEm: this.dateProvider.convertToUtc3Hours(new Date()),
+            observacoes: "Conta 1",
+            numeroParcelas: 10,
+            numeroParcelasAtual: 10,
+            valorInicial: 100,
+            valorParcela: 10,
+            valorAtual: 100,
+            dataVencimentoInicial: this.dateProvider.addDays(30),
+            dataVencimentoAtual: this.dateProvider.addDays(30),
+            dataVencimentoFinal: this.dateProvider.addMonths(
+                this.dateProvider.addDays(30),
+                10
+            ),
+            fkIdLojista: lojista2.id,
+            fkIdCliente: cliente2.id,
         },
     });
 
